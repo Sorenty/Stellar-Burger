5c654167b0daaabc37b324de4be8b25cf1003d2b
Aleksandr <aleksnadry@example.com>
2025-06-03 19:21:49 +0300

    refactor: вынес повторяющиеся селекторы в константы

--- cypress/e2e/tests.cy.tsx
+++ cypress/e2e/tests.cy.tsx
@@ -1,21 +1,37 @@
-// Повторяющиеся селекторы
+// cypress/e2e/tests.cy.tsx
+
+// Вынесли все повторяющиеся селекторы в константы
 const SELECTORS = {
+  // API endpoints
   userApi: '/api/auth/user',
   ingredientsApi: '/api/ingredients',
   orderApi: '/api/orders',
+
+  // Поля и тексты
   userNameInput: 'input[name="name"]',
   testUserName: 'Test User',
   profileUrlPart: '/profile',
   homeUrl: 'http://localhost:4000/',
+
+  // Ингредиенты
   bunName: 'Флюоресцентная булка R2-D3',
   fillingName: 'Биокотлета из марсианской Магнолии',
   bunOption: 'Краторная булка',
   bunPlaceholder: 'Выберите булки',
   fillingPlaceholder: 'Выберите начинку',
+  ingredientsTab: 'Начинки',
+
+  // Кнопки и тексты
   orderButton: 'Оформить заказ',
   orderConfirmationText: 'идентификатор заказа',
   constructorTitle: 'Соберите бургер',
   personalCabinet: 'Личный кабинет',
+
+  // Общие селекторы-литералы (body, нажатие Esc, координаты клика)
+  bodyTag: 'body',
+  escapeKey: '{esc}',
+  clickX: 10,
+  clickY: 10,
 };
 
 describe('Авторизация и профиль', () => {
@@ -27,8 +43,8 @@ describe('Авторизация и профиль', () => {
         user: {
           email: 'test_user@example.com',
           name: SELECTORS.testUserName,
-        }
-      }
+        },
+      },
     }).as('getUser');
 
     cy.loginByApi();
@@ -49,15 +65,15 @@ describe('Функциональность конструктора бургер
     cy.fixture('user.json').as('userData');
 
     cy.intercept('GET', SELECTORS.ingredientsApi, {
-      fixture: 'ingredients.json'
+      fixture: 'ingredients.json',
     }).as('getIngredients');
 
     cy.intercept('GET', SELECTORS.userApi, {
-      fixture: 'user.json'
+      fixture: 'user.json',
     }).as('getUser');
 
     cy.setCookie('accessToken', 'mockToken');
-    cy.window().then(win => {
+    cy.window().then((win) => {
       win.localStorage.setItem('refreshToken', 'mockToken');
     });
 
@@ -71,25 +87,25 @@ describe('Функциональность конструктора бургер
   });
 
   it('Добавление булки в конструктор', () => {
-    cy.contains(SELECTORS.bunName).next().click();
+    cy.contains(SELECTORS.bunName).next('button').click();
     cy.contains(SELECTORS.bunName, { timeout: 10000 }).should('exist');
   });
 
   it('Добавление начинки в конструктор', () => {
-    cy.contains('Начинки').scrollIntoView().click({ force: true });
-    cy.contains(SELECTORS.fillingName).next().click();
+    cy.contains(SELECTORS.ingredientsTab).scrollIntoView().click({ force: true });
+    cy.contains(SELECTORS.fillingName).next('button').click();
     cy.contains(SELECTORS.fillingName).should('exist');
   });
 
   it('Добавление ингредиентов в заказ и очистка конструктора', () => {
     cy.intercept('POST', SELECTORS.orderApi, {
       fixture: 'makeOrder.json',
-      statusCode: 200
+      statusCode: 200,
     }).as('newOrder');
 
-    cy.contains(SELECTORS.bunName).next().click();
-    cy.contains('Начинки').scrollIntoView();
-    cy.contains(SELECTORS.fillingName).next().click();
+    cy.contains(SELECTORS.bunName).next('button').click();
+    cy.contains(SELECTORS.ingredientsTab).scrollIntoView();
+    cy.contains(SELECTORS.fillingName).next('button').click();
 
     cy.contains(SELECTORS.orderButton).should('not.be.disabled').click();
     cy.wait('@newOrder', { timeout: 30000 })
@@ -97,20 +113,20 @@ describe('Функциональность конструктора бургер
       .should('eq', 200);
 
     cy.contains(SELECTORS.orderConfirmationText).should('be.visible');
-    cy.get('body').type('{esc}');
+    cy.get(SELECTORS.bodyTag).type(SELECTORS.escapeKey);
     cy.contains(SELECTORS.bunPlaceholder).should('exist');
   });
 
   it('Открытие и закрытие модального окна ингредиента', () => {
     cy.contains(SELECTORS.bunOption).click();
     cy.url().should('include', '/ingredients/');
-    cy.get('body').type('{esc}');
+    cy.get(SELECTORS.bodyTag).type(SELECTORS.escapeKey);
     cy.url().should('eq', SELECTORS.homeUrl);
   });
 
   it('Закрытие модального окна через клик на оверлей', () => {
     cy.contains(SELECTORS.bunOption).click();
-    cy.get('body').click(10, 10);
+    cy.get(SELECTORS.bodyTag).click(SELECTORS.clickX, SELECTORS.clickY);
     cy.url().should('eq', SELECTORS.homeUrl);
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,45 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('loginAndStubUser', () => {
+  cy.request('POST', 'https://norma.nomoreparties.space/api/auth/login', {
+    email: 'test_user@example.com',
+    password: '12345678'
+  }).then((res) => {
+    const accessToken = res.body.accessToken.split('Bearer ')[1];
+    const refreshToken = res.body.refreshToken;
+
+    cy.setCookie('accessToken', accessToken);
+    cy.window().then((win) => {
+      win.localStorage.setItem('refreshToken', refreshToken);
+    });
+
+    cy.intercept('GET', '**/api/auth/user', {
+      statusCode: 200,
+      body: {
+        success: true,
+        user: {
+          email: 'test_user@example.com',
+          name: 'Test User'
+        }
+      }
+    }).as('getUser');
+
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+  });
+});
+
+Cypress.Commands.add('stubOrderAndCheckout', () => {
+  cy.intercept('POST', '**/api/orders', {
+    fixture: 'makeOrder.json',
+    statusCode: 200
+  }).as('newOrder');
+});
+
 Cypress.Commands.add('loginByApi', () => {
   cy.request('POST', 'https://norma.nomoreparties.space/api/auth/login', {
     email: 'test_user@example.com',
-
     password: '12345678'
   }).then((res) => {
     const accessToken = res.body.accessToken.split('Bearer ')[1];
@@ -24,6 +62,7 @@ Cypress.Commands.add('loginByApi', () => {
     }).as('getUser');
   });
 });
+
 
 Cypress.on('window:before:load', (win) => {
   cy.spy(win, 'fetch').as('fetchSpy');
